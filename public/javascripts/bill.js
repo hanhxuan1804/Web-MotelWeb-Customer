c863e435665f909404a416fc715ee91bcdbc097c
vanDattt <82921584+vanDattt@users.noreply.github.com>
2022-01-19 10:46:03 +0700

    alert total price

---
+++ public/javascripts/bill.js
@@ -0,0 +1,26 @@
+var url = window.location.href;
+if(url.indexOf('page=')==-1 || url.indexOf('page=1')>-1){
+  document.getElementById('Prev_button').style.display = "none"
+}
+
+function CheckValidBill(price) {
+    if(datepicker.value.length==0||
+        datepicker1.value.length==0||
+        ((numAdults==0)&&(numChilds==0)))
+      alert("Don't leave any blank box");
+    else{
+      const date1 = new Date(datepicker.value);
+      const date2 = new Date(datepicker1.value);
+
+      const diffTime = Math.abs(date2 - date1);
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+      const total=price*diffDays;
+
+      if(total/1000000>0){
+        alert("Create successful, price = " + total/1000000 + " trieu VND");
+      }
+      else{
+        alert("Create successful, price = " + total/1000 + "nghin VND");
+      }
+    }  
+  };
